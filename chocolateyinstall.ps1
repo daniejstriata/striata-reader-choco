947ff1533c2294d1ba1991740bb7c2140e3c88eb
daniejstriata <13314239+daniejstriata@users.noreply.github.com>
2021-11-03 14:15:30 +0200

    Add files via upload

---
+++ chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'striata-reader'
+$fileType = 'exe'
+$url = 'https://reader.striata.com/downloads/Windows/64bit/striata-reader.exe'
+
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "packageName" "$fileType" "$silentArgs" "$url"  -validExitCodes  $validExitCodes  -checksum "d6ef312ca423b36251c212c846218e20" -checksumType "md5"
